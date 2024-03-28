d53bb23e20fd5dd7f87b1fb92472a0e9ea25355f
Irell Zane <itsirellzane@gmail.com>
2024-03-28 14:26:39 +0000

    Start Ex2 and edit Ex1 pdf

--- Ex1/data_viz_code.R
+++ Ex1/data_viz_code.R
@@ -11,5 +11,5 @@ labeled_countries_data = subset(data_subset, Country %in% labeled_countries)
 ggplot(data_subset) +
   geom_point(size = 3, alpha=0.7, aes(GDP, HCI, color = Continent)) +
   geom_text(data = labeled_countries_data, aes(GDP, HCI, label = Country), vjust = 2.7) +
-  geom_segment(data = labeled_countries_data, aes(x = GDP, y = HCI - 0.02, xend = GDP, yend = HCI),  # Adding leader lines
+  geom_segment(data = labeled_countries_data, aes(x = GDP, y = HCI - 0.02, xend = GDP, yend = HCI),  
                arrow = arrow(length = unit(0.3, "cm")), color = "black")
