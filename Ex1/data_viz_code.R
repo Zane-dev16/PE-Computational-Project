d6488e10bb20b1a536feb1d7f117ae99a7923e16
Irell Zane <itsirellzane@gmail.com>
2024-03-29 11:27:21 +0000

    Edit labels on plots

--- Ex1/data_viz_code.R
+++ Ex1/data_viz_code.R
@@ -1,14 +1,14 @@
-library(readr)
 library(ggplot2)
 
 continents = c("Europe", "Americas")
 labeled_countries = c("Lithuania", "Iceland", "United States", "Saint Lucia")
 
-gdp_hci_data <- read_csv("Paises_PIB_ICH.csv")
-data_subset = subset(gdp_hci_data, Continent %in% continents)
-labeled_countries_data = subset(data_subset, Country %in% labeled_countries)
+gdp_hci_data <- read.csv("Paises_PIB_ICH.csv")
+selected_data = subset(gdp_hci_data, Continent %in% continents)
+labeled_countries_data = subset(selected_data, Country %in% labeled_countries)
 
-ggplot(data_subset) +
+ggplot(selected_data) +
+  labs(title="GDP per Capita vs. HCI", x="GDP per capita adjusted for the cost of living", y="Human Capital Index") +
   geom_point(size = 3, alpha=0.7, aes(GDP, HCI, color = Continent)) +
   geom_text(data = labeled_countries_data, aes(GDP, HCI, label = Country), vjust = 2.7) +
   geom_segment(data = labeled_countries_data, aes(x = GDP, y = HCI - 0.02, xend = GDP, yend = HCI),  
--- Ex2/data_viz_code.R
+++ Ex2/data_viz_code.R
@@ -5,5 +5,7 @@ master_data <- read.csv("master.csv")
 selected_data <- subset(master_data, year == 1986 & age == "25-34 years")
 
 ggplot(data=selected_data, aes(x=suicides.100k.pop, y=sex, color=sex)) +
-  labs(title="Suicides per 100k population by sex") +
+  labs(title = "Suicides per 100k population by sex in several countries",
+       x = "Suicides per 100k population",
+       y = NULL) +
   geom_boxplot()
