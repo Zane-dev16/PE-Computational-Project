532f9429dd11082d3f97add1e8d6db1d9749bd55
Irell Zane <itsirellzane@gmail.com>
2024-03-29 10:30:46 +0000

    Complete exercise 2

--- Ex2/data_viz_code.R
+++ Ex2/data_viz_code.R
@@ -1,2 +1,9 @@
-master <- read.csv("master.csv")
-View(master)
+library(ggplot2)
+
+master_data <- read.csv("master.csv")
+
+selected_data <- subset(master_data, year == 1986 & age == "25-34 years")
+
+ggplot(data=selected_data, aes(x=suicides.100k.pop, y=sex, color=sex)) +
+  labs(title="Suicides per 100k population by sex") +
+  geom_boxplot()
