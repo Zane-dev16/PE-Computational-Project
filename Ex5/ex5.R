2cf6a461b4131a05524239231c8698c2dd99b82a
Irell Zane <itsirellzane@gmail.com>
2024-04-04 11:21:57 +0100

    Complete ex5

--- Ex5/ex5.R
+++ Ex5/ex5.R
@@ -3,3 +3,19 @@ seed <- 1950
 r <- 300
 m <- 170
 
+set.seed(1950)
+
+proportions <- numeric(23)
+for (i in 1:r) {
+  num_lower <- 0
+  for (j in 1:m) {
+    T = sqrt(n) * rnorm(1) / sqrt(sum(rnorm(n - 1)^2))
+    if (T <= 1.5) {
+      num_lower = num_lower + 1
+    }
+  }
+  proportions[i] <- num_lower / m
+}
+
+mean(proportions)
+pt(1.5, df=n)
